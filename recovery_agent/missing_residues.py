9d11ca61578d0e83174c9a6452820a91e671f8d5
nakamura26002002921-a11y <nakamura26002002921@gmail.com>
2026-07-20 17:54:09 +0900

    Update missing_residues.py

--- recovery_agent/missing_residues.py
+++ recovery_agent/missing_residues.py
@@ -1,7 +1,6 @@
 # recovery_agent/missing_residues.py
 from pdbfixer import PDBFixer
 
-
 def count_missing_residues(pdb_path):
     """PDBFixerで検出できる欠損残基の総数を返す"""
     fixer = PDBFixer(filename=pdb_path)
